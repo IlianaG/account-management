f833f8b097dfd5815dc0b5602637524abdf0ec81
Francisco Jimenez <fran@192.168.1.3>
2023-08-24 21:09:31 -0600

    Adding HTTP examples

--- src/main/java/com/dsm/mx/api/HelloWorldPaco.java
+++ src/main/java/com/dsm/mx/api/HelloWorldPaco.java
@@ -1,7 +1,9 @@
 package com.dsm.mx.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dsm.mx.dto.Account;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 public class HelloWorldPaco {
@@ -10,4 +12,33 @@ public class HelloWorldPaco {
     public String helloPaco(){
         return "Buenas noches Paco";
     }
+
+    // GET http://localhost:8080/home/accounts --all accounts from current user
+    // c123, c456, c789
+    // GET http://localhost:8080/home/accounts/c456/details --- access details for a specific account
+    // GET http://localhost:8080/home/accounts?creditCards=true
+
+    // GET http://localhost:8080/signup?name=Francisco&email=sdfsd@gmail.com&password=12345....
+    // POST
+
+    //PathParam o PathVariable
+    @GetMapping(value = "/hola/{nombre}")
+    public String helloPerson(@PathVariable("nombre") String name, @RequestParam("flag1") String flag1){
+        return "Buenas noches--" + name + "   " + flag1;
+    }
+
+    //Request parameters after the ? symbol. Example ?name=paco&email=myemail&password=securepass
+    @GetMapping(value = "/signup")
+    public String signup(@RequestParam String name, @RequestParam String email, @RequestParam String password){
+        System.out.println("name:" +  name +  " email:" + email + " password:" + password);
+        return "Tu registro fue exitoso";
+    }
+
+    @PostMapping(value = "/accounts")
+    public String newAccount(@RequestBody Account account){
+        System.out.println("Se recibe: " + account);
+        return "Cuenta registrada";
+    }
+
+
 }
